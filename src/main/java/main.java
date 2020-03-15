2e75bd3732526c58f3b78f8a22fd74d1806f0832
enderent <miniurwisek@gmail.com>
2020-03-15 01:43:56 +0100

    aaa

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,26 +1,69 @@
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.utils.Compression;
-import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import net.dv8tion.jda.api.managers.AudioManager;
 
 import javax.security.auth.login.LoginException;
-import java.util.EnumSet;
 
 public class main extends ListenerAdapter {
+    static char prfix = ',';
+    static boolean isclaimed = true;
+
+    public static void main(String[] args) throws LoginException {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+
+        String token = "Njg4NDc2MTQ1MTYyNzgwNzAw.Xm1H0A.3n85MK47mYs84NVeVWoTBrNUKhw";
+        builder.setToken(token);
+        builder.addEventListeners(new main());
+        builder.setActivity(Activity.watching("Killing of false gods"));
+
+        //lavaplayer impementation
+        AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
+        AudioSourceManagers.registerRemoteSources(playerManager);
+        AudioPlayer player = playerManager.createPlayer();
+
+        builder.build();
 
 
+    }
 
-        public static void main(String[] args) throws LoginException {
-            JDABuilder builder = new JDABuilder(AccountType.BOT);
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 
-            String token ="Njg4NDc2MTQ1MTYyNzgwNzAw.Xm03jw.lW8WG0by2OfjBe0Th3Q5vZtSh1c";
-            builder.setToken(token);
-
-            builder.build();
-
+        if (event.getMessage().getContentRaw().equals(prfix + "Tek'ma'tek Teal'c")) {
+            event.getChannel().sendMessage("Tek'ma'tek!" + " " + event.getAuthor().getName()).queue();
         }
 
+        if (event.getMessage().getContentDisplay().contains(".claim")) {
 
-}
+            if (!event.getAuthor().isBot()) {
+                event.getChannel().sendMessage("Hasshak").queue();
+                //isclaimed = false;
+            } //else
+                //isclaimed = true;
+            if (event.getMessage().getContentRaw().startsWith(prfix+"play")) {
+
+                VoiceChannel channel = event.getMember().getVoiceState().getChannel();
+                AudioManager manager = event.getGuild().getAudioManager();
+                manager.openAudioConnection(channel);
+
+
+
+
+
+            }
+
+
+        }
+    }
+
+
+    }
+
